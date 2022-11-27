63194be4903d8fc36d272ecf8106306abee132c2
Abdullah661382 <AbdullahAdel711@gmail.com>
2022-11-27 19:25:46 +0000

    hw5

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 def Quadratic():
-    d = (b**2) - (4*a*c)
-    return (- b - d**0.5)/(2*a)
-
-def Quadratic2():
     e = (b**2) - (4*a*c)
-    return (- b - e**0.5)/(2*a)
+    return (- b + e**0.5)/(2*a)
 while True:
     try:
-        a = input("Give me a value of a ")
-        b = input("Give me a value of b ")
-        c = input("Give me a value of c ")
-        if Quadratic() > 0 and Quadratic2() > 0:
-            print("Thank you", Quadratic(), Quadratic2())
+        a = int(input("Give me a value of a "))
+        b = int(input("Give me a value of b "))
+        c = int(input("Give me a value of c "))
+        if Quadratic() > 0:
+            print("Thank you", Quadratic())
             break
     except:
         print("I don't understand please try again. ")
